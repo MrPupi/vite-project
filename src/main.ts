505c4d9dbd2722252198689358123fc18493573d
mrpupi <miromannz@mail.ru>
2024-02-20 21:04:51 +0300

    hfaethfshs

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-import './style.css'
+import './style.scss'
+
+console.log ('Hello, Peep')
+
+const nameUser = prompt(' Укажите ваше Имя') 
+const surnameUser = prompt(' Укажите вашу Фамилию') 
+const ageUser = prompt(' Укажите ваш Возраст') 
+
+alert(`Всё верно? ${nameUser} ${surnameUser} ${ageUser}`)
+
+
+
