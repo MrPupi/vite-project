b8b062196b8a52a0f4069e5758f7784ac54c2d88
MrPupi <miromannz@mail.ru>
2024-02-21 17:04:38 +0300

    fawefsadgfar

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import './style.scss'
 
 console.log ('Hello, Peep')
 
-const nameUser = prompt(' Укажите ваше Имя') 
-const surnameUser = prompt(' Укажите вашу Фамилию') 
-const ageUser = prompt(' Укажите ваш Возраст') 
+// const nameUser = prompt(' Укажите ваше Имя') 
+// const surnameUser = prompt(' Укажите вашу Фамилию') 
+// const ageUser = prompt(' Укажите ваш Возраст') 
 
-alert(`Всё верно? ${nameUser} ${surnameUser} ${ageUser}`)
+// alert(`Всё верно? ${nameUser} ${surnameUser} ${ageUser}`)
 
+// console.log ('я этого тайпскрипта рот ебал!!!!')
 
+type Euro = number|string
+let did = ('Укажите количество Долларов')
+const euro: Euro = 0.927582 
+const dollars = parseInt(prompt('did', 25))
+let message = euro*dollars 
+function conventure(message:number) {
+  console.log (message)
+}
+console.log (message)
 
+alert(message)
