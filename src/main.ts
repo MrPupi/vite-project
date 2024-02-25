dc3d86525e887ce5e6c33ed5673b357ee0be0888
mrpupi <miromannz@mail.ru>
2024-02-26 02:19:34 +0300

    njfbghiad

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,49 @@ console.log ('Hello, Peep')
 // alert(`Всё верно? ${nameUser} ${surnameUser} ${ageUser}`)
 
 // console.log ('я этого тайпскрипта рот ебал!!!!')
+const rates = {}
+const elementUSD = document.querySelector('[data-value="USD"]') 
 
-type Euro = number|string
-let did = ('Укажите количество Долларов')
-const euro: Euro = 0.927582 
-const dollars = parseInt(prompt('did', 25))
-let message = euro*dollars 
-function conventure(message:number) {
-  console.log (message)
+const elementEUR = document.querySelector('[data-value="EUR"]')
+
+const elementGBP = document.querySelector('[data-value="GBP"]')
+
+getCurrent();  
+
+
+async function getCurrent() {
+  const responsive = await fetch ('https://www.cbr-xml-daily.ru/daily_json.js');
+  const data = await responsive.json();
+  const result = await data
+  console.log(result)
+
+  console.log(result.Valute.USD.Value)
+
+  console.log(result.Valute.EUR.Value)
+
+  console.log(result.Valute.GBP.Value)
+
+  rates.USD = result.Valute.USD
+
+  rates.EUR = result.Valute.EUR
+
+  rates.GBP = result.Valute.GBP
+
+  console.log(rates)
+
+  elementUSD.textContent = rates.USD.Value.toFixed(2);
+
+  elementEUR.textContent = rates.EUR.Value.toFixed(2);
+
+  elementGBP.textContent  = rates.GBP.Value.toFixed(2);
+
+
+  if (rates.USD.Value > rates.USD.Previous) {
+    elementUSD?.classList.add('top')
+  } else {
+    elementUSD?.classList.add('bottom')
+  }
+
 }
-console.log (message)
 
-alert(message)
+
