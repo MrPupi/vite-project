3359a64a8fda41fef9fe315b4c549491bb4d4e04
mrpupi <miromannz@mail.ru>
2024-02-26 04:03:20 +0300

    птвспрврп

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,55 @@
-import './style.scss'
+import "./style.scss";
 
-console.log ('Hello, Peep')
+console.log("Hello, Peep");
 
-// const nameUser = prompt(' Укажите ваше Имя') 
-// const surnameUser = prompt(' Укажите вашу Фамилию') 
-// const ageUser = prompt(' Укажите ваш Возраст') 
+// const nameUser = prompt(' Укажите ваше Имя')
+// const surnameUser = prompt(' Укажите вашу Фамилию')
+// const ageUser = prompt(' Укажите ваш Возраст')
 
 // alert(`Всё верно? ${nameUser} ${surnameUser} ${ageUser}`)
-
+interface myObj {
+  [key: string]: { Value: number; Previous: number };
+}
 // console.log ('я этого тайпскрипта рот ебал!!!!')
-const rates = {}
-const elementUSD = document.querySelector('[data-value="USD"]') 
+const rates: myObj = {};
+const elementUSD = document.querySelector('[data-value="USD"]');
 
-const elementEUR = document.querySelector('[data-value="EUR"]')
+const elementEUR = document.querySelector('[data-value="EUR"]');
 
-const elementGBP = document.querySelector('[data-value="GBP"]')
-
-getCurrent();  
-
+const elementGBP = document.querySelector('[data-value="GBP"]');
+getCurrent();
 
 async function getCurrent() {
-  const responsive = await fetch ('https://www.cbr-xml-daily.ru/daily_json.js');
+  if (!elementEUR || !elementGBP || !elementUSD)
+    throw new Error("Не найден элемент, соси хуй!");
+  const responsive = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
   const data = await responsive.json();
-  const result = await data
-  console.log(result)
+  const result = await data;
+  // console.log(result)
 
-  console.log(result.Valute.USD.Value)
+  // console.log(result.Valute.USD.Value)
 
-  console.log(result.Valute.EUR.Value)
+  // console.log(result.Valute.EUR.Value)
 
-  console.log(result.Valute.GBP.Value)
+  // console.log(result.Valute.GBP.Value)
 
-  rates.USD = result.Valute.USD
+  rates.USD = result.Valute.USD;
 
-  rates.EUR = result.Valute.EUR
+  rates.EUR = result.Valute.EUR;
 
-  rates.GBP = result.Valute.GBP
+  rates.GBP = result.Valute.GBP;
 
-  console.log(rates)
+  console.log(rates);
 
   elementUSD.textContent = rates.USD.Value.toFixed(2);
 
   elementEUR.textContent = rates.EUR.Value.toFixed(2);
 
-  elementGBP.textContent  = rates.GBP.Value.toFixed(2);
-
+  elementGBP.textContent = rates.GBP.Value.toFixed(2);
 
   if (rates.USD.Value > rates.USD.Previous) {
-    elementUSD?.classList.add('top')
+    elementUSD?.classList.add("top");
   } else {
-    elementUSD?.classList.add('bottom')
+    elementUSD?.classList.add("bottom");
   }
-
 }
-
-
